7b1cc3329009cc530eb67ead7d94b5cfebbb47a1
RoJinx <mar.lewandowski@gmail.com>
2022-11-23 10:49:22 +0100

    errors handling, and connection to DB

--- index.ts
+++ index.ts
@@ -1,12 +1,20 @@
-import express, {json} from "express";
+import express, {json, Request, Response} from "express";
 import cors from 'cors';
-import 'express-async-errors'
+import 'express-async-errors';
+import {handleError} from "./utils/errors";
 
 const app = express();
 app.use(cors({
   origin: 'http://localhost:3000',
 }))
 app.use(json());
+
+app.get('/', (req :Request,res: Response)=>{
+  throw new Error('Damn');
+})
+
+
+app.use(handleError);
 
 app.listen(3001, '0.0.0.0',()=>{
   console.log('Listening on port http://localhost:3001...');
